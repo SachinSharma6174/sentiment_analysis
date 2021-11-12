aee6a94f7b6f56d6d0aa44bce033655fb6b33346
sachin sharma <sachin.sharma6174@gmail.com>
2021-11-12 16:21:37 -0700

    final working code push

--- rest/rest-server.py
+++ rest/rest-server.py
@@ -4,7 +4,10 @@ import platform
 import io, os, sys
 import pika, redis
 import hashlib, requests
+import jsonpickle
 import json
+
+app = Flask(__name__)
 
 ##
 ## Configure test vs. production
@@ -12,7 +15,102 @@ import json
 redisHost = os.getenv("REDIS_HOST") or "localhost"
 rabbitMQHost = os.getenv("RABBITMQ_HOST") or "localhost"
 
-print("Connecting to rabbitmq({}) and redis({})".format(rabbitMQHost,redisHost))
+# print("Connecting to rabbitmq({}) and redis({})".format(rabbitMQHost,redisHost))
+
+# redis = redis.Redis(
+#      host= '10.106.190.124',
+#      port= '6379')
+
+redis = redis.Redis(
+     host= 'localhost',
+     port= '6379')
+
+
+# redis.set('mykey', 'Hello from Docker!')
+# redis.set('This thing sucks','{"analysis":{"model":"sentiment","result":{"entities":[],"labels":[{"confidence":0.9999688863754272,"value":"NEGATIVE"}],"text":"This thing sucks"}},"sentence":"This thing sucks"}')
+redis.set('I don\'t think this one exists','{"analysis":{"model":"sentiment","result":{"entities":[],"labels":[{"confidence":0.9999688863754272,"value":"NEGATIVE"}],"text":"I don\'t think this one exists"}},"sentence":"I don\'t think this one exists"}')
+value = redis.get('mykey') 
+print(value)
+
+
+@app.route('/apiv1/analyze', methods=['POST'])
+def analyze():
+    r = request
+    request_pickled = jsonpickle.encode(r.get_json())
+    # convert the data to a PIL image type so we can extract dimensions
+    try:
+    	rabbitMQ = pika.BlockingConnection(
+    		pika.ConnectionParameters(host=rabbitMQHost))
+    	rabbitMQChannel = rabbitMQ.channel()
+    	rabbitMQChannel.queue_declare(queue='toWorker')
+    	rabbitMQChannel.exchange_declare(exchange='logs', exchange_type='topic')
+    	infoKey = f"{platform.node()}.worker.info"
+    	debugKey = f"{platform.node()}.worker.debug"
+    	# formattedJson = json.dumps(workerJson)
+    	rabbitMQChannel.basic_publish(exchange='',routing_key='toWorker', body=request_pickled)
+    	response = {
+    		"action": "queued" 
+        }
+    except Exception as e:
+        response = { "error": e }
+        print(e)
+    response_pickled = jsonpickle.encode(response)
+    return Response(response=response_pickled, status=200, mimetype="application/json")
+
+@app.route('/apiv1/cache/', methods=['GET'])
+def cache():
+    keys = redis.keys()
+    value = []
+    for key in keys:
+    	value.append(redis.get(key).decode())
+
+    # convert the data to a PIL image type so we can extract dimensions
+    try:
+        response = {
+            'value' : value
+            }
+    except:
+        response = { 'value' : 0}
+    response_pickled = jsonpickle.encode(response)
+    return Response(response=response_pickled, status=200, mimetype="application/json")
+
+@app.route('/apiv1/sentiment/sentence', methods=['POST'])
+def sentiment():
+    r = request
+    try:
+    	req = r.get_json()
+    	model = req['model']
+    	data = req['sentences']
+    	print(model)
+    	print(data)
+    	sentences=[]
+
+    	for sentence in data:
+    		if redis.exists(sentence):
+    			sentences.append(redis.get(sentence).decode())
+
+    	response = {
+    		"model": "sentiment",
+        	"sentences": sentences
+  		 }    
+    except Exception as e:
+    	
+        response = { 
+        	"error" : "not analyzed yet :("
+        }
+        print(e)
+
+
+    response_pickled = jsonpickle.encode(response)
+    return Response(response=response_pickled, status=200, mimetype="application/json")
+
+
+@app.route('/')
+def hello_world():
+	return "200 OK"
+    # return render_template('index.html')
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0',port=5000)
 
 ##
 ## Your code goes here..
--- worker/worker-server.py
+++ worker/worker-server.py
@@ -2,6 +2,7 @@
 # Worker server
 #
 import pickle
+import jsonpickle
 import platform
 import io
 import os
@@ -12,24 +13,28 @@ import hashlib
 import json
 import requests
 
-from flair.models import TextClassifier
+
 from flair.data import Sentence
 
+from flair.models import TextClassifier
+classifier = TextClassifier.load('sentiment')
 
 hostname = platform.node()
 
 ##
 ## Configure test vs. production
 ##
-redisHost = os.getenv("REDIS_HOST") or "localhost"
-rabbitMQHost = os.getenv("RABBITMQ_HOST") or "localhost"
+#redisHost = os.getenv("REDIS_HOST") or "localhost"
+#rabbitMQHost = os.getenv("RABBITMQ_HOST") or "localhost"
+redisHost='10.1.0.15'
+rabbitMQHost='10.1.0.19'
 
 print(f"Connecting to rabbitmq({rabbitMQHost}) and redis({redisHost})")
 
 ##
 ## Set up redis connections
 ##
-db = redis.Redis(host=redisHost, db=1)                                                                           
+db = redis.Redis(host=redisHost)                                                                           
 
 ##
 ## Set up rabbitmq connection
@@ -38,14 +43,44 @@ rabbitMQ = pika.BlockingConnection(
         pika.ConnectionParameters(host=rabbitMQHost))
 rabbitMQChannel = rabbitMQ.channel()
 
-rabbitMQChannel.queue_declare(queue='toWorker')
-rabbitMQChannel.exchange_declare(exchange='logs', exchange_type='topic')
+# rabbitMQChannel.queue_declare(queue='toWorker')
+# rabbitMQChannel.exchange_declare(exchange='logs', exchange_type='topic')
 infoKey = f"{platform.node()}.worker.info"
 debugKey = f"{platform.node()}.worker.debug"
+
+
+def callback(ch, method, properties, body):
+    print("Received %r" % body.decode())
+    req = eval(body.decode())
+    print(req)
+    model = req['model']
+    sentences = req['sentences']
+    # sentence_dict = {}
+
+    for sentence in sentences:
+
+        if not db.exists(sentence):
+            sentence_to_predict = Sentence(sentence)
+            classifier.predict(sentence_to_predict)
+            print(sentence_to_predict.to_dict('sentiment'))
+            db.set(sentence,str(sentence_to_predict.to_dict('sentiment')))   
+            # sentence_dict[sentence] = sentence_to_predict.to_dict('sentiment')
+
+    ch.basic_ack(delivery_tag = method.delivery_tag)
+
+
+# rabbitMQChannel.basic_consume(queue='toWorker',auto_ack=false,on_message_callback=callback)
+rabbitMQChannel.basic_consume(queue='toWorker',auto_ack=False ,on_message_callback=callback)
+rabbitMQChannel.start_consuming()
+
+
+
+
 def log_debug(message, key=debugKey):
     print("DEBUG:", message, file=sys.stdout)
     rabbitMQChannel.basic_publish(
         exchange='logs', routing_key=key, body=message)
+
 def log_info(message, key=infoKey):
     print("INFO:", message, file=sys.stdout)
     rabbitMQChannel.basic_publish(
